f2f8f62cbf717ce94fd7e6a73385d9c3b4b3d4b3
Alberto Lorente <a.lorenteleal@gmail.com>
2015-09-26 17:53:15 +0200

    Adding the constraints requested for ten elements and filter out older than 10 days.

--- src/main/java/com/dwview/profileviewer/storage/MockDataStoreImplementation.java
+++ src/main/java/com/dwview/profileviewer/storage/MockDataStoreImplementation.java
@@ -1,11 +1,15 @@
 package com.dwview.profileviewer.storage;
 
 import com.dwview.profileviewer.representations.View;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeComparator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * Created by Alberto on 2015-09-26.
@@ -21,7 +25,13 @@ public class MockDataStoreImplementation implements DataStore {
     @Override
     public void createView(long whoViewedId,View view) {
         if(storage.containsKey(whoViewedId)){
-            storage.get(whoViewedId).add(view);
+            List<View> views = storage.get(whoViewedId);
+            views.add(view);
+            Collections.sort(views, (o1, o2) -> {
+                DateTime o1Date = new DateTime(o1.getDateTime());
+                DateTime o2Date = new DateTime(o1.getDateTime());
+                return DateTimeComparator.getInstance().compare(o1Date,o2Date);
+            });
         }else{
             List<View> views = new ArrayList<>();
             views.add(view);
@@ -31,9 +41,14 @@ public class MockDataStoreImplementation implements DataStore {
 
     @Override
     public List<View> listViews(long viewerId) {
+        List<View> views = null;
         if(storage.containsKey(viewerId)){
-            return storage.get(viewerId);
+            views = storage.get(viewerId);
+            views = views.stream().filter(view ->
+                    (new DateTime(view.getDateTime())).isBefore(DateTime.now().plusDays(10)))
+                    .limit(10)
+                    .collect(Collectors.toList());
         }
-        return null;
+        return views;
     }
 }
